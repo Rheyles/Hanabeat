25d7dea544033f20b8725780acd150e9262c96d3
Rheyles <148101401+Rheyles@users.noreply.github.com>
2024-06-18 10:11:42 +0200

    Clean Resources folders and creation of Detonator class

--- Scenes/Detonator/Detonator.gd
+++ Scenes/Detonator/Detonator.gd
@@ -1,11 +1,38 @@
 extends Node2D
 
+const spark_scene = preload("res://Scenes/Spark/Spark.tscn")
+
+@onready var spark_spawn = $SparkSpawn
+@onready var click_area = $ClickArea
+
+var mouse_is_in : bool = false
+
+### BUILT-IN
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	click_area.mouse_entered.connect(_on_ClickArea_mouse_entered)
+	click_area.mouse_exited.connect(_on_ClickArea_mouse_exited)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _process(_delta):
+	if Input.is_action_just_pressed('click') and mouse_is_in:
+		# If there is mouse click on the detonator, we create a spark
+		create_spark()
+
+### LOGIC
+
+func create_spark() -> void:
+	# Create a spark at the spawn
+	var spark = spark_scene.instantiate()
+	add_child(spark)
+	spark.transform = spark_spawn.transform
+	return
+
+### SIGNAL RESPONSES
+
+func _on_ClickArea_mouse_entered() -> void:
+	mouse_is_in = true
+
+func _on_ClickArea_mouse_exited() -> void:
+	mouse_is_in = false
--- Scenes/FuseNode/FuseNode.gd
+++ Scenes/FuseNode/FuseNode.gd
@@ -6,5 +6,6 @@ func _renameAtInstantiate():
 	self.name = "FuseNode_" + str(linePointRef)
 
 func _checkForOtherFuseNode():
-#At instantiate => Check for colision with other FuseNode Collider ? Instance "knot sprite" for visual fb ?
-#At each step of burning => Check for colision with other FuseNode Collider, if it return a FuseNode, burn every Node returned.
+	#At instantiate => Check for colision with other FuseNode Collider ? Instance "knot sprite" for visual fb ?
+	#At each step of burning => Check for colision with other FuseNode Collider, if it return a FuseNode, burn every Node returned.
+	pass
