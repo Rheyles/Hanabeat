f6d866282a63acd2c05cabdfd830d674878cb90a
Rheyles <148101401+Rheyles@users.noreply.github.com>
2024-07-05 15:34:30 +0200

    add decorations (herbs)

---
+++ Scenes/Playground/Decorations/herb.gd
@@ -0,0 +1,10 @@
+extends Sprite2D
+
+
+func _ready():
+	var rng = RandomNumberGenerator.new()
+	rng.randomize()
+	randomize()
+	var offset = rng.randf_range(0.0,0.5)
+	self.material.set_shader_parameter("Offset", offset)
+
