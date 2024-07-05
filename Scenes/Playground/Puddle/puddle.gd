2c7451df974e0353fd250167c09058695ee83c22
Rheyles <148101401+Rheyles@users.noreply.github.com>
2024-07-05 15:52:05 +0200

    add puddle sprite

--- Scenes/Playground/Puddle/puddle.gd
+++ Scenes/Playground/Puddle/puddle.gd
@@ -1,11 +1,16 @@
 extends Area2D
 
 @export var is_water : bool = true
+@export var oil_modulate : Color
 
 @onready var shape_cast = $ShapeCast2D
 
 func _ready():
 	self.area_entered.connect(_on_area_entered)
+	if is_water:
+		modulate = Color.WHITE
+	else:
+		modulate = oil_modulate
 
 func _on_area_entered(area:Area2D)->void:
 	if is_water:
