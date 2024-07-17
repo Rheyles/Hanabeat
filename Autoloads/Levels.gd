0f394f4ce41f40c03a713823f53a9f71b5cfebe1
Sakdos <edouard.qb@laposte.net>
2024-07-17 19:59:26 +0200

    Lvl 2 tweak, lvl 4 & 6 added

--- Autoloads/Levels.gd
+++ Autoloads/Levels.gd
@@ -4,9 +4,10 @@ extends Node
 ## This autoload contains all the method and variable used to manage levels
 
 var levels = ["res://Scenes/Playground/_Test/AmazingScene.tscn",
-			  "res://Scenes/Levels/level_1.tscn",
+			"res://Scenes/Levels/level_1.tscn",
 			  "res://Scenes/Levels/level_2_bis.tscn",
 			  "res://Scenes/Levels/level_3_bis.tscn",
 			  "res://Scenes/Levels/level_4.tscn",
-			  "res://Scenes/Levels/level_5.tscn"]
+			  "res://Scenes/Levels/level_5.tscn",
+			  "res://Scenes/Levels/level_6.tscn"]
 
