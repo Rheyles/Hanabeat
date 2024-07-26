672afaee582e603be5d2f8366d93ccc179e07919
Sakdos <edouard.qb@laposte.net>
2024-07-26 18:11:28 +0200

    New dialog Sprite & floating Island

--- Autoloads/Levels.gd
+++ Autoloads/Levels.gd
@@ -14,11 +14,11 @@ var levels = ["res://Scenes/Playground/_Test/AmazingScene.tscn",
 var level_dict : Dictionary = {
 	0:[0,2], 
 	1:[0,2], 
-	2:[0,2], 
-	3:[0,0,2], 
+	2:[0,1], 
+	3:[1,0,2], 
 	4:[0,2,0], 
 	5:[0,2], 
-	6:[2,0,2]}
+	6:[0,1,2]}
 
 #Sprite mood : 0 = Basic, 1 = Unhappy, 2 = Laughing
 
